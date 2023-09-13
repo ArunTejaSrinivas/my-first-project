e019b955670b5e6fa080f0dd7c4add49c5bed45f
ArunTejaSrinivas <srinivasaruntejathorlikonda@gmail.com>
2023-09-13 10:57:23 +0530

    Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
-}
+  Teja = {
+    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-L_rj8CuYehsE3IHUWe3zUw7St8gvHSYObw&usqp=CAU",
+    movieName : "OPPENHEIMER",
+    rating : "⭐ 9.1",
+    description : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti molestias,
+                   sequi repellendus eaque libero voluptatem itaque consequuntur facere illum
+                   rem quis error eum, repellat, rerum dolorum earum! Nihil, harum similique.
+                   Architecto autem porro sunt culpa nesciunt fugit sit illum, quisquam quam,
+                   impedit at odio magni distinctio quos ut qui voluptate`
+  }
+
+  Arun = {
+    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-L_rj8CuYehsE3IHUWe3zUw7St8gvHSYObw&usqp=CAU",
+    movieName : "OPPENHEIMER",
+    rating : "⭐ 9.1",
+    description : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti molestias,
+                   sequi repellendus eaque libero voluptatem itaque consequuntur facere illum
+                   rem quis error eum, repellat, rerum dolorum earum! Nihil, harum similique.
+                   Architecto autem porro sunt culpa nesciunt fugit sit illum, quisquam quam,
+                   impedit at odio magni distinctio quos ut qui voluptate`
+  }
+
+
+  Srinivas = {
+    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-L_rj8CuYehsE3IHUWe3zUw7St8gvHSYObw&usqp=CAU",
+    movieName : "OPPENHEIMER",
+    rating : "⭐ 9.1",
+    description : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti molestias,
+                   sequi repellendus eaque libero voluptatem itaque consequuntur facere illum
+                   rem quis error eum, repellat, rerum dolorum earum! Nihil, harum similique.
+                   Architecto autem porro sunt culpa nesciunt fugit sit illum, quisquam quam,
+                   impedit at odio magni distinctio quos ut qui voluptate`
+  }
+
+  Thorlikonda = {
+    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-L_rj8CuYehsE3IHUWe3zUw7St8gvHSYObw&usqp=CAU",
+    movieName : "OPPENHEIMER",
+    rating : "⭐ 9.1",
+    description : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti molestias,
+                   sequi repellendus eaque libero voluptatem itaque consequuntur facere illum
+                   rem quis error eum, repellat, rerum dolorum earum! Nihil, harum similique.
+                   Architecto autem porro sunt culpa nesciunt fugit sit illum, quisquam quam,
+                   impedit at odio magni distinctio quos ut qui voluptate`
+  }
+
+  movies = ['Teja', 'Srinivas', 'Arun', 'Thorlikonda']
+
+  }
